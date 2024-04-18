0fa3ae3efab5d52de909798391ec05bf281c278d
lelesf99 <viniciuslelesf@gmail.com>
2024-04-18 16:24:15 -0300

    flask_socketio

--- api/app.py
+++ api/app.py
@@ -1,7 +1,7 @@
 import mmap
-from flask import Flask, request, jsonify, send_from_directory
 import os
-import threading
+from flask import Flask, request, jsonify, send_from_directory
+from flask_socketio import SocketIO, emit
 from models import db, Document
 from flask_cors import CORS
 
@@ -13,6 +13,7 @@ app.config['MAX_CONTENT_LENGTH'] = 8 * 1000 * 1000 * 1000  # 16 Gigabyte limit
 
 db.init_app(app)
 CORS(app)
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 
 # In-memory storage for tracking file chunks
@@ -24,7 +25,7 @@ def list_files():
     files = Document.query.all()
     return jsonify([file.serialize() for file in files])
 
-@app.route('/search', methods=['GET'])
+@app.route('/search_old', methods=['GET'])
 def search_files():
     search_query = request.args.get('query')
     if(search_query == None or search_query == ""):
@@ -59,6 +60,9 @@ def search_files():
                 
     return jsonify(search_results)
 
+
+
+
 # download file
 @app.route('/uploaded_files/<path:file_name>', methods=['GET'])
 def download_file(file_name):
@@ -80,56 +84,67 @@ def delete_file():
         return jsonify({'message': 'File deleted'})
     return jsonify({'message': 'File not found'})
 
-@app.route('/upload_chunk', methods=['POST'])
-def upload_chunk():
-    chunk = request.files['chunk']
-    chunk_number = int(request.form['chunkNumber'])
-    file_name = request.form['fileName']
-    folder_name = file_name.split('.')[0]
-    total_chunks = int(request.form['totalChunks'])
-
-    # Ensure the directory exists
-    file_dir = os.path.join(app.config['UPLOAD_FOLDER'], folder_name)
-    if not os.path.exists(file_dir):
-        os.makedirs(file_dir, mode=0o755, exist_ok=True)
-
-    # Save chunk to disk
-    chunk_path = os.path.join(file_dir, f"{chunk_number:04d}.part")
-    with open(chunk_path, 'wb') as f:
-        f.write(chunk.read())
-
-    if file_name not in file_chunks:
-        file_chunks[file_name] = set()
-    file_chunks[file_name].add(chunk_number)
-    
-    if len(file_chunks[file_name]) == total_chunks:
-        # Assemble the file from chunks
-        threading.Thread(target=assemble_file, args=(file_name, total_chunks, file_dir)).start()
-        file_chunks[file_name] = set()
-
-    return jsonify({'message': f'Chunk {chunk_number} received'})
-
-def assemble_file(file_name, total_chunks, directory):
-    target_file_path = os.path.join(app.config['UPLOAD_FOLDER'], file_name)
-    with open(target_file_path, 'wb') as target_file:
-        for i in range(1, total_chunks + 1):
-            part_path = os.path.join(directory, f"{i:04d}.part")
-            with open(part_path, 'rb') as part_file:
-                target_file.write(part_file.read())
-            os.remove(part_path)  # Optional: Remove part file after assembly
-        os.rmdir(directory)  # Remove the directory after assembly
-
-    print(f'File {file_name} has been assembled from {total_chunks} parts.')
-    add_file_db(file_name, target_file_path)
-        
-def add_file_db(file_name, file_path):
-    app.app_context().push()
-    if(Document.query.filter_by(name=file_name).first()):
-        return
-    new_file = Document(name=file_name, path=file_path)
-    db.session.add(new_file)
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    file = request.files['file']
+    file_name = file.filename
+    file_path = os.path.join(app.config['UPLOAD_FOLDER'], file_name)
+    file.save(file_path)
+    # add or update file to database
+    new_file = Document.query.filter_by(name=file_name).first()
+    if new_file:
+        new_file.path = file_path
+    else:
+        new_file = Document(name=file_name, path=file_path)
+        db.session.add(new_file)
     db.session.commit()
 
+    return jsonify(new_file.serialize())
 
+@socketio.on('connect')
+def handle_connect():
+    print('Client connected')
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    print('Client disconnected')
+
+@socketio.on('search')
+def handle_search(query):
+    files = Document.query.all()
+    total_bytes = 0
+    byte_counter = 0
+    for file in files:
+        total_bytes += os.path.getsize(file.path)
+    emit('search_progress', {'searched': 0,'totalBytes': total_bytes})
+    for file in files:
+        if(os.path.isfile(file.path)):
+            with open(os.path.realpath(file.path), 'rb', 0) as file_bytes:
+                s = mmap.mmap(file_bytes.fileno(), 0, access=mmap.ACCESS_READ)
+                # convert search_query to bytes
+                search_bytes = query.encode('utf-8')
+                # search for every occurence of search_query in the file
+                index = s.find(search_bytes)
+                if(index != -1):
+                    emit('result', {"fileName": file.name, "file_dbPath": file.path, "occurences": []})    
+                    while index != -1:
+                        # save string near occurence
+                        emit('occurence', {"fileName": file.name, "occurence": {
+                                "start": index,
+                                "end": index + len(search_bytes),
+                                "context": s[index - 20:index + 20].decode('utf-8', errors='ignore')
+                            }
+                        })
+                        emit('search_progress', {'searched': byte_counter + index,'totalBytes': total_bytes})
+                        index = s.find(search_bytes, index + 1)
+                byte_counter += os.path.getsize(file.path)
+                emit('search_progress', {'searched': byte_counter + index,'totalBytes': total_bytes})
+    emit('search_progress', {'searched': total_bytes,'totalBytes': total_bytes})
+    emit('close_connection', 'done')
+
+# debug mode
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    socketio.run(app, debug=True)
+
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=5000)
